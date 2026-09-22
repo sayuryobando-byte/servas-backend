fe5079a3584a584e777d737f585e0501887d4cf3
Sayury <sayury.obando@gmail.com>
2026-09-21 23:15:47 -0500

    Fix: habilitar CORS en controladores

--- src/main/java/com/servas/web/catalog/CatalogController.java
+++ src/main/java/com/servas/web/catalog/CatalogController.java
@@ -19,6 +19,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 public class CatalogController {
